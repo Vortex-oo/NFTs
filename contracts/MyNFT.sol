687b755e365d606e5c503a644f3d419691ad08ea
Vortex-oo <debjit.github@gmail.com>
2025-09-20 09:49:05 +0530

    .

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -1,10 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol"
-import "@openzeppelin/contracts/utils/Counters/Counters.sol"
-import "@openzeppelin/contracts/access/Ownable.sol"
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
+contract MyNFT is ERC721URIStorage, Ownable {
 
+    uint256 private _tokenId;
+
+    constructor () ERC721("Vortex", "VTX") Ownable(msg.sender) {}
+    
+    function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256) {
+            _tokenId++;
+            uint256 newItemId = _tokenId;
+            _mint(recipient, newItemId);    
+            _setTokenURI(newItemId, tokenURI);
+            return newItemId;
+        }
+}
